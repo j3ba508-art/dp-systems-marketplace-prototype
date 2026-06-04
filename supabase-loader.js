3ca8e136182afa2381d044d781cd0edd9ce084c0
j3ba508-art <j3ba508@gmail.com>
2026-06-04 12:12:23 +0800

    added index,html and missing loader file

---
+++ supabase-loader.js
@@ -0,0 +1,12 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL = "https://uomfboryzwfnubvgabyg.supabase.co";
+const SUPABASE_KEY = "sb_publishable_7WUa8dw75nonkPIMWOTWqQ_lrgJPZLz";
+
+// Create ONE global instance
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Attach globally for non-module scripts
+window.supabaseClient = supabase;
+
+console.log("Supabase READY");
